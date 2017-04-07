5660acaec0f2eb9f3db25103143948b4d89d47e7
ssefick <ssefick@gmail.com>
2017-04-07 07:40:13 -0500

    data added for genotypeR

---
+++ R/genotypes_data.R
@@ -0,0 +1,22 @@
+#' Genotyping data from the sequenom platform
+#' from markers produced with genotypeR
+#'
+#' Data from a recombination plasticity experiment in 
+#' Drosophila pseudoobscura. This data is provided to demonstrate the
+#' the use of the genotypeR package
+#'
+#' @docType data
+#'
+#' @usage data(genotypes_data)
+#'
+#' @format An object read in with \code{read_in_sequenom_data}; see \code{\link[genotypeR]{read_in_sequenom_data}}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(genotypes_data)
+#' head(genotypes_data)
+#' colnames(genotypes_data)
+#' 
+"genotypes_data"
