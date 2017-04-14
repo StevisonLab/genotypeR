fb8f849e3465dd12f54b4d8b6031845078285f70
mcastronova <mac0079@auburn.edu>
2017-04-14 00:16:42 -0500

    Update Pipeline_Rapper.sh

--- inst/SequenomMarkers/Pipeline_Rapper.sh
+++ inst/SequenomMarkers/Pipeline_Rapper.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 # Uses vcftools to compare samples
-# Input the file names in zipped format on the command line.
+# Input the file names in an unzipped format on the command line.
 # Created: April 3, 2017
-# Last Updated: April 3, 2017
+# Last Updated: April 14, 2017
 # Author: Magdalena Castronova
 # vcftools v0.1.14+
 
-##module load vcftools
+module load vcftools
 
 echo "Starting step one..."
 vcftools --vcf $1 --diff $2 --diff-site --out Sample1_v_Sample2.out
-echo "Step one complete!\n"
+echo "Step one complete!"
 echo "Starting step two..."
-./Finding_SNPs.pl Sample1_v_Sample2.out.diff.sites_in_files
-echo "Step two complete!\n"
+if [ $3 == "gg" ]; then
+	./Finding_SNPs_GoldenGate.pl Sample1_v_Sample2.out.diff.sites_in_files
+	#echo "I chose gg"
+else
+	if [ $3 == "sq" ]; then
+		./Finding_SNPs_Sequenom.pl Sample1_v_Sample2.out.diff.sites_in_files
+		#echo "I chose sq"
+	fi
+fi
+echo "Step two complete!"
 echo "Starting step three..."
 ./Grandmaster_SNPs.pl $1 $2
-echo "Step three complete!\n"
+echo "Step three complete!"
